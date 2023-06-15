f94e3aa1d886f9230f5871aac159361d3bdd7c2e
citec-47 <mauricendonyi40@gmail.com>
2023-06-15 00:05:58 -0700

    fix linters

--- morse_decode.rb
+++ morse_decode.rb
@@ -1,103 +1,60 @@
-$output = []
+output = []
+
+MORSE_CODE = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
 def decode_char(ch)
-   if(ch=='.-')
-    return 'A'
-   elsif(ch=='-...')
-    return 'B'
-   elsif(ch=='-.-.')
-    return 'C'
-   elsif(ch=='-..')
-    return 'D'
-   elsif(ch=='.')
-    return 'E'
-   elsif(ch=='..-.')
-    return 'F'
-   elsif(ch=='--.')
-    return 'G'
-   elsif(ch=='....')
-    return 'H'
-   elsif(ch=='..')
-    return 'I'
-   elsif(ch=='.---')
-    return 'J'
-   elsif(ch=='-.-')
-    return 'K'
-   elsif(ch=='.-..')
-    return 'L'
-   elsif(ch=='--')
-    return 'M'
-   elsif(ch=='-.')
-    return 'N'
-   elsif(ch=='---')
-    return 'O'
-   elsif(ch=='.--.')
-    return 'P'
-   elsif(ch=='--.-')
-    return 'Q'
-   elsif(ch=='.-.')
-    return 'R'
-   elsif(ch=='...')
-    return 'S'
-   elsif(ch=='-')
-    return 'T'
-   elsif(ch=='..-')
-    return 'U'
-   elsif(ch=='...-')
-    return 'V'
-   elsif(ch=='.--')
-    return 'W'
-   elsif(ch=='-..-')
-    return 'X'
-   elsif(ch=='-.--')
-    return 'Y'
-   elsif(ch=='--..')
-    return 'Z'
-   elsif(ch=='.----')
-    return '1'
-   elsif(ch=='..---')
-    return '2'
-   elsif(ch=='...--')
-    return '3'
-   elsif(ch=='....-')
-    return '4'
-   elsif(ch=='.....')
-    return '5'
-   elsif(ch=='-...')
-    return '6'
-   elsif(ch=='--...')
-    return '7'
-   elsif(ch=='---..')
-    return '8'
-   elsif(ch=='----.')
-    return '9'
-   elsif(ch=='-----')
-    return '0'
-   end
- end
+  MORSE_CODE[ch]
+end
 
- def decode_word (word)
+def decode_word(word)
   $output = []
   chars = word.split('')
   combine = ''
-  chars.each_with_index { |n, idx|
-   if(n!=' ')
-    if(idx==chars.length-1)
-     combine += n
-     $output.push(combine)
+
+  chars.each_with_index do |n, idx|
+    if n != ' '
+      if idx == chars.length - 1
+        combine += n
+        $output.push(combine)
+      else
+        combine += n
+      end
     else
-     combine += n
+      $output.push(combine) unless combine.empty?
+      combine = ''
     end
-   else
-    $output.push(combine)
-    combine = ''
-   end
- }
- answer = '';
- $output.each { |n|
- answer += decode_char(n).to_s
- }
- return answer
+  end
+
+  answer = ''
+  $output.each { |n| answer += decode_char(n).to_s }
+  answer
 end
 
 def decode_message(message)
@@ -105,6 +62,7 @@ def decode_message(message)
   count = 0
   chars = message.split('')
   msg = ''
+
   chars.each_with_index do |n, idx|
     if n == ' ' && chars[idx + 1] == ' ' && chars[idx + 2] == ' '
       msg += decode_word(word).to_s
@@ -119,5 +77,6 @@ def decode_message(message)
       end
     end
   end
+
   print(msg)
 end
